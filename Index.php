1e512eae776152fb7f1a9340acf4cdf88edfa34d
Sven Bosman <73939667+Sven-Bosman@users.noreply.github.com>
2024-03-25 13:49:58 +0100

    V0.0.2

--- Index.php
+++ Index.php
@@ -8,33 +8,35 @@
 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/main.css">
     <title>Beroeps_Vitaliteit</title>
 </head>
 <body>
     <header>
         <nav>
             <ul>
-                <li><button>Form</button></li>
-                <li><button>samenVatting</button></li>
-                <li><a href="php/gegevens.php">Gegevens</a></li>
+                <li class="h-list"><button>Form</button></li>
+                <li class="h-list"><button>samenVatting</button></li>
+                <li class="h-list"><a href="php/gegevens.php">Gegevens</a></li>
             </ul>
         </nav>
     </header>
+<main>    
     <div class="batterij" id="batterij-1"></div>
     <div class="batterij" id="batterij-2"></div>
     <div class="batterij" id="batterij-3"></div>
 
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-    Naam: <input type="text" name="naam" required><br>
-    E-mail: <input type="email" name="email" required><br>
-    Bericht: <textarea name="bericht" required></textarea><br>
-    <input type="submit" value="Verzenden">
-</form>
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+        Naam: <input type="text" name="naam" required><br>
+        E-mail: <input type="email" name="email" required><br>
+        Bericht: <textarea name="bericht" required></textarea><br>
+        <input type="submit" value="Verzenden">
+    </form>
 
 
-    <button type="submit">Submit</button>
-</form>
-
+        <button type="submit">Submit</button>
+    </form>
+</main>
 </body>
 
 <script src="js/main.js">
