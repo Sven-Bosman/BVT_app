cd8fe9065de735e355bb99e1ec8772ab7faa049e
Sven Bosman <73939667+Sven-Bosman@users.noreply.github.com>
2024-04-10 13:18:09 +0200

    batterijen link
    link voor de batterijen in main

--- Index.php
+++ Index.php
@@ -9,6 +9,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/baterijen.css">
+    
     <title>Beroeps_Vitaliteit</title>
 </head>
 <body>
